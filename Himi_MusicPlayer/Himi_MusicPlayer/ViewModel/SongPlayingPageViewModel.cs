4a73f85d72798ac9119e20bde8117e646fb312a8
thinhvuthecreator <thinhvuh@gmail.com>
2023-07-26 16:49:31 +0700

    demo giao dien choi nhac

--- Himi_MusicPlayer/Himi_MusicPlayer/ViewModel/SongPlayingPageViewModel.cs
+++ Himi_MusicPlayer/Himi_MusicPlayer/ViewModel/SongPlayingPageViewModel.cs
@@ -1,11 +1,32 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Himi_MusicPlayer.ViewModel
 {
     public partial class SongPlayingPageViewModel : ObservableObject
     {
+        bool isPlaying = true;
         public SongPlayingPageViewModel() 
         { 
+        }
+
+        
+
+        [RelayCommand]
+        void Play_Pause_Click(ImageButton button)
+        {
+           
+            if (isPlaying == true)
+            {
+                button.Source = "play.png";
+                button.Padding = 15;
+                isPlaying = false;
+            }
+            else
+            {
+                button.Source = "pause.png";
+                isPlaying = true;
+            }
         }
     }
 }
